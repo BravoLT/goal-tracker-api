ab7943bc0556d070559282273714d0f4673d9f36
David Schoutens <david.schoutens@bravolt.com>
2017-03-11 20:43:40 -0500

    Renamed project assets to 'goal-tracker' from 'neocons-example'

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject neocons-example "0.1.0-SNAPSHOT"
+(defproject goal-tracker "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
@@ -6,6 +6,6 @@
                  [clojurewerkz/neocons "3.1.0"]
                  [ring/ring-json "0.5.0-beta1"]]
   :plugins [[lein-ring "0.11.0"]]
-  :ring {:handler neocons-example.handler/app}
+  :ring {:handler goal-tracker.handler/app}
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                                   [ring-mock "0.1.5"]]}})
